0cf5135687af33831514a2e278439ea28a52b8c8
Robert Black <robertblack@Roberts-MacBook-Air.local>
2016-02-12 12:44:45 -0500

    buggy js

--- javascript/game.js
+++ javascript/game.js
@@ -1,48 +1,54 @@
-//    Initial values    ------------>
-var wins = 0;
-var wi = 0;       //  <----word index used for choosing current word
-var words = ["spider","baboon","gorilla","howler","macaque","pygmy","mandrill","rhesus","orangutan"];
+		//    Initial values    ------------>
+window.onload = function init() {
+	var wins = 0;
+	var currentBoard = "_____";
+	var usedLetters = [];
+	var currentWord = "spider";
+	var li = 0;
+	var guesses = 12;
+	var wi = 0;       //  <----word index used for choosing current word
+	var words = ["spider","baboon","gorilla","howler","macaque","pygmy","mandrill","rhesus","orangutan"];
+}
 //  function called when a player wins or loses a game ----->
 function newGame() {
-	var guesses = 12;
-	var usedLetters = [];
-	var currentWord = words[wi];
-	var currentBoard = "";
-	var li = 0;      // <--letter index used for assigning values to used letters array
+	guesses = 12;
+	usedLetters = [];
+	currentWord = words[wi];
+	currentBoard = "";
+	li = 0;      // <--letter index used for assigning values to used letters array
 	//   creates a blank board for the current word ---->
 	for (var j=0; j<currentWord.length; j++){
 		currentBoard += "_";
 	}
-	document.querySelector(#board).innerHTML = currentBoard;
-	document.querySelector(#wins).innerHTML = "Wins: "+wins;
-	document.querySelector(#used-letters).innerHTML = "";
-
+	document.querySelector("#board").innerHTML = currentBoard;
+	document.querySelector("#wins").innerHTML = "Wins: "+wins;
+	document.querySelector("#used-letters").innerHTML = "";
+	document.querySelector("#guesses").innerHTML = "12";
 }
 //   checks if player won current game   ----->
 function winCheck() {
-	var win = true;
-	for(var g=0; g<currentBoard.length; g++){
+	for(var g=0; g < currentBoard.length; g++){
 		if(currentBoard[g]=="_"){
-			win = false;
+			return false;
 		}   
 	}
+	return true;
 }
 //   Function initiated when key is pressed
 document.onkeyup = function(event) {
 	//   first checks if player lost or won and starts new game if they have  --->
 	if (guesses == 0){
 		wi++;
-		.newGame();
+		newGame();
 		return;
 	}
-	.winCheck();
-	if(win==true){
+	if(winCheck()){
 		wins++;
 		wi++;
-		.newGame();
+		newGame();
 		return;
 	}
-	var currentLetter = String.fromCharCode(event.keyCode).toLowercase();
+	var currentLetter = String.fromCharCode(event.keyCode).toLowerCase();
 	// checks if current letter had already been used  ----->
 	for (var t=0; t<usedLetters.length; t++){  
 		if (currentLetter==usedLetters[t]){
@@ -51,16 +57,15 @@ document.onkeyup = function(event) {
 	}
 	//  checks if current letter is in the current word   ---->
 	for (var i=0; i<currentWord.length; i++){
-		if (currentWord[i]==currentLetter){
+		if (currentWord[i]===currentLetter){
 			//   reveals current letter in the play board  ----->
-			currentBoard[i] = currentLetter.toUppercase();
+			currentBoard[i] = currentLetter.toUpperCase();
 		}
 	}
 	usedLetters[li] = currentLetter; // <---  adds letter to used letter array
 	li++;
 	guesses--;
-	document.querySelector(#board) = currentBoard;
-	
-	document.querySelector(#used-letters) = usedLetters
-	document.querySelector(#guesses) = "Guesses left:" + guesses;
+	document.querySelector("#board").innerHTML = currentBoard;
+	document.querySelector("#used-letters").innerHTML = usedLetters;
+	document.querySelector("#guesses").innerHTML = "Guesses left:" + guesses;
 }
